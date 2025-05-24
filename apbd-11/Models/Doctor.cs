c9468d40297329b5daa5dd22733b98b104c85d8a
IvanMazhara <s31385@pjwstk.edu.pl>
2025-05-24 14:43:13 +0200

    Doctor model implementation.

--- apbd-11/Models/Doctor.cs
+++ apbd-11/Models/Doctor.cs
@@ -1,6 +1,11 @@
-﻿namespace apbd_11.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apbd_11.Models;
 
 public class Doctor
 {
-    
+    [Key] public int IdDoctor { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
 }
