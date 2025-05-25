18e40c35143a9d1e0d64a5377ab314c09fa964f7
IvanMazhara <s31385@pjwstk.edu.pl>
2025-05-25 23:38:33 +0200

    Services final implementation

--- apbd-11/Repositories/DbService.cs
+++ apbd-11/Repositories/DbService.cs
@@ -1,29 +1,90 @@
 ﻿using apbd_11.Data;
+using apbd_11.DTOs;
 using apbd_11.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace apbd_11.Repositories;
 
 public class DbService : IDbService
 {
     private readonly DatabaseContext _context;
+
     public DbService(DatabaseContext context)
     {
         _context = context;
     }
-    
-    public async Task<List<Prescription>> GetAllPrescriptions()
+
+    public async Task<Patient?> GetPatientDetails(int id)
     {
-        throw new NotImplementedException();
+        var patient = await _context.Patients
+            .Include(p => p.Prescriptions)
+            .ThenInclude(pr => pr.Doctor)
+            .Include(p => p.Prescriptions)
+            .ThenInclude(pr => pr.PrescriptionMedicaments)
+            .ThenInclude(pm => pm.Medicament)
+            .FirstOrDefaultAsync(p => p.IdPatient == id);
+
+        if (patient == null)
+            return null;
+        
+        patient.Prescriptions = patient.Prescriptions.OrderBy(p => p.DueDate).ToList();
+
+        return patient;
     }
 
-    public async Task<Patient> GetPatient(int Id)
+    public async Task<bool> AddPrescription(PrescriptionDto prescriptionDto)
     {
-        var patient = await _context.Patients.Select(e => { });
-        throw new NotImplementedException();
-    }
+        if (prescriptionDto.DueDate < prescriptionDto.Date)
+            throw new ArgumentException("DueDate must be greater than or equal to Date.");
 
-    public Task<Prescription> addPrescription(Prescription prescription)
-    {
-        throw new NotImplementedException();
+        if (prescriptionDto.Medicaments.Count > 10)
+            throw new ArgumentException("Cannot assign more than 10 medicaments.");
+
+        var doctor = await _context.Doctors.FindAsync(prescriptionDto.IdDoctor);
+        if (doctor == null)
+            throw new ArgumentException("Doctor does not exist.");
+
+        var patient = await _context.Patients.FindAsync(prescriptionDto.IdPatient);
+        if (patient == null)
+        {
+            if (prescriptionDto.Patient == null)
+                throw new ArgumentException("Patient does not exist and no data provided to create one.");
+
+            patient = new Patient
+            {
+                FirstName = prescriptionDto.Patient.FirstName,
+                LastName = prescriptionDto.Patient.LastName,
+                Birthdate = prescriptionDto.Patient.Birthdate
+            };
+            _context.Patients.Add(patient);
+            await _context.SaveChangesAsync();
+        }
+
+        var prescription = new Prescription
+        {
+            Date = prescriptionDto.Date,
+            DueDate = prescriptionDto.DueDate,
+            IdDoctor = doctor.IdDoctor,
+            IdPatient = patient.IdPatient,
+            PrescriptionMedicaments = new List<PrescriptionMedicament>()
+        };
+
+        foreach (var medDto in prescriptionDto.Medicaments)
+        {
+            var medicament = await _context.Medicaments.FindAsync(medDto.IdMedicament);
+            if (medicament == null)
+                throw new ArgumentException($"Medicament with ID {medDto.IdMedicament} does not exist.");
+
+            prescription.PrescriptionMedicaments.Add(new PrescriptionMedicament
+            {
+                IdMedicament = medicament.IdMedicament,
+                Dose = medDto.Dose,
+                Details = medDto.Description
+            });
+        }
+
+        _context.Prescriptions.Add(prescription);
+        await _context.SaveChangesAsync();
+        return true;
     }
 }
--- apbd-11/Repositories/IDbService.cs
+++ apbd-11/Repositories/IDbService.cs
@@ -1,11 +1,10 @@
-﻿using apbd_11.Models;
+﻿using apbd_11.DTOs;
+using apbd_11.Models;
 
 namespace apbd_11.Repositories;
 
 public interface IDbService
 {
-    Task<List<Prescription>> GetAllPrescriptions();
-    Task<Patient> GetPatient(int Id);
-    Task<Prescription> addPrescription(Prescription prescription);
-    
+    Task<Patient?> GetPatientDetails(int id);
+    Task<bool> AddPrescription(PrescriptionDto prescriptionDto);
 }
