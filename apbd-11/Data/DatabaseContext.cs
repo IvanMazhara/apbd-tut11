bebbcd8d0b2244bba9f8d798f3d72bc4ade885f2
IvanMazhara <s31385@pjwstk.edu.pl>
2025-05-25 20:49:56 +0200

    DbContext update

--- apbd-11/Data/DatabaseContext.cs
+++ apbd-11/Data/DatabaseContext.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using apbd_11.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace apbd_11.Data;
 
 public class DatabaseContext : DbContext
 {
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Medicament> Medicaments { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Prescription> Prescriptions { get; set; }
+    public DbSet<PrescriptionMedicament> PrescriptionMedicaments { get; set; }
     DatabaseContext()
+    {
+    }
+    public DatabaseContext(DbContextOptions options) : base(options)
     {
     }
 }
