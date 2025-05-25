e9ca474c08344e4377db837538e4940f4459dd58
IvanMazhara <s31385@pjwstk.edu.pl>
2025-05-25 20:50:08 +0200

    DbService update

--- apbd-11/Repositories/DbService.cs
+++ apbd-11/Repositories/DbService.cs
@@ -1,6 +1,29 @@
-﻿namespace apbd_11.Repositories;
+﻿using apbd_11.Data;
+using apbd_11.Models;
+
+namespace apbd_11.Repositories;
 
 public class DbService : IDbService
 {
+    private readonly DatabaseContext _context;
+    public DbService(DatabaseContext context)
+    {
+        _context = context;
+    }
     
+    public async Task<List<Prescription>> GetAllPrescriptions()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Patient> GetPatient(int Id)
+    {
+        var patient = await _context.Patients.Select(e => { });
+        throw new NotImplementedException();
+    }
+
+    public Task<Prescription> addPrescription(Prescription prescription)
+    {
+        throw new NotImplementedException();
+    }
 }
--- apbd-11/Repositories/IDbService.cs
+++ apbd-11/Repositories/IDbService.cs
@@ -1,6 +1,11 @@
-﻿namespace apbd_11.Repositories;
+﻿using apbd_11.Models;
+
+namespace apbd_11.Repositories;
 
 public interface IDbService
 {
+    Task<List<Prescription>> GetAllPrescriptions();
+    Task<Patient> GetPatient(int Id);
+    Task<Prescription> addPrescription(Prescription prescription);
     
 }
