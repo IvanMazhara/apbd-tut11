d2c4732eb572079dd9f453db0868fc7b994178c8
IvanMazhara <s31385@pjwstk.edu.pl>
2025-05-25 23:40:29 +0200

    Primitive database context implementation

--- apbd-11/Data/DatabaseContext.cs
+++ apbd-11/Data/DatabaseContext.cs
@@ -13,7 +13,71 @@ public class DatabaseContext : DbContext
     DatabaseContext()
     {
     }
-    public DatabaseContext(DbContextOptions options) : base(options)
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
     {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Patient>().ToTable("Patient");
+        modelBuilder.Entity<Doctor>().ToTable("Doctor");
+        modelBuilder.Entity<Medicament>().ToTable("Medicament");
+        modelBuilder.Entity<Prescription>().ToTable("Prescription");
+        modelBuilder.Entity<PrescriptionMedicament>().ToTable("PrescriptionMedicament");
+
+        modelBuilder.Entity<Doctor>().HasData(new Doctor
+        {
+            IdDoctor = 1,
+            FirstName = "Vic",
+            LastName = "Morrow",
+            Email = "vicmor1967@gmail.com"
+        });
+
+        modelBuilder.Entity<Patient>().HasData(
+            new Patient
+            {
+                IdPatient = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Birthdate = new DateTime(1966, 03, 01)
+            },
+            new Patient
+            {
+                IdPatient = 2,
+                FirstName = "Alice",
+                LastName = "Smith",
+                Birthdate = new DateTime(1985, 3, 10)
+            },
+            new Patient
+            {
+                IdPatient = 3,
+                FirstName = "Bob",
+                LastName = "Johnson",
+                Birthdate = new DateTime(1990, 8, 22)
+            },
+            new Patient
+            {
+                IdPatient = 4,
+                FirstName = "Llewelyn",
+                LastName = "Moss",
+                Birthdate = new DateTime(1978, 12, 5)
+            },
+            new Patient
+            {
+                IdPatient = 5,
+                FirstName = "Monica",
+                LastName = "Lee",
+                Birthdate = new DateTime(2000, 6, 14)
+            }
+        );
+
+        
+        modelBuilder.Entity<Medicament>().HasData(new Medicament
+        {
+            IdMedicament = 1,
+            Name = "Ibuprofen",
+            Description = "Take pill once a day.",
+            Type = "Nonsteroidal anti-inflammatory drug (NSAID)"
+        });
     }
 }
---
+++ apbd-11/Data/DatabaseContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using apbd_11.Data;
+
+public class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+{
+    public DatabaseContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Apbd11Db;Trusted_Connection=True;");
+
+        return new DatabaseContext(optionsBuilder.Options);
+    }
+}
