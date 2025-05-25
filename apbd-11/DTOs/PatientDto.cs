cac80596ffc136914e787fe3d46481e6d1d4e278
IvanMazhara <s31385@pjwstk.edu.pl>
2025-05-25 20:49:36 +0200

    Dto's added

---
+++ apbd-11/DTOs/PatientDto.cs
@@ -0,0 +1,8 @@
+﻿namespace apbd_11.DTOs;
+
+public class PatientDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
---
+++ apbd-11/DTOs/PrescriptionDto.cs
@@ -0,0 +1,23 @@
+﻿namespace apbd_11.DTOs;
+
+public class PrescriptionDto
+{
+    public int IdPrescription { get; set; }
+    public DateTime Date { get; set; }
+    public DateTime DueDate { get; set; }
+    public DoctorDto Doctor { get; set; }
+    public List<MedicamentDto> Medicaments { get; set; }
+}
+public class DoctorDto
+{
+    public int IdDoctor { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+public class MedicamentDto
+{
+    public int IdMedicament { get; set; }
+    public int? Dose { get; set; }
+    public string Description { get; set; }
+}
+
