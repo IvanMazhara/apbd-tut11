3e02f9614f20a23f9a674dd06a9b2ef7fb699cff
IvanMazhara <s31385@pjwstk.edu.pl>
2025-05-25 23:38:50 +0200

    Dtos

--- apbd-11/DTOs/PatientDto.cs
+++ apbd-11/DTOs/PatientDto.cs
@@ -4,5 +4,5 @@ public class PatientDto
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public DateTime DateOfBirth { get; set; }
+    public DateTime Birthdate { get; set; }
 }
--- apbd-11/DTOs/PrescriptionDto.cs
+++ apbd-11/DTOs/PrescriptionDto.cs
@@ -2,22 +2,25 @@
 
 public class PrescriptionDto
 {
-    public int IdPrescription { get; set; }
+    public int IdDoctor { get; set; }
+    public int IdPatient { get; set; }
     public DateTime Date { get; set; }
     public DateTime DueDate { get; set; }
-    public DoctorDto Doctor { get; set; }
-    public List<MedicamentDto> Medicaments { get; set; }
+    public List<MedicamentDto> Medicaments { get; set; } = new();
+    public PatientDto? Patient { get; set; }
 }
+
+public class MedicamentDto
+{
+    public int IdMedicament { get; set; }
+    public int? Dose { get; set; }
+    public string Description { get; set; } = string.Empty;
+}
+
 public class DoctorDto
 {
     public int IdDoctor { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-}
-public class MedicamentDto
-{
-    public int IdMedicament { get; set; }
-    public int? Dose { get; set; }
-    public string Description { get; set; }
 }
 
