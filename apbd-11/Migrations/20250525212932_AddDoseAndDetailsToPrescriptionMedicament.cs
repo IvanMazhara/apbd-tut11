18f55b31e663b0ef93ac468f84017ab4137cee4e
IvanMazhara <s31385@pjwstk.edu.pl>
2025-05-25 23:41:08 +0200

    Migrations

---
+++ apbd-11/Migrations/20250525212932_AddDoseAndDetailsToPrescriptionMedicament.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace apbd_11.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDoseAndDetailsToPrescriptionMedicament : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Details",
+                table: "PrescriptionMedicament",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Dose",
+                table: "PrescriptionMedicament",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Details",
+                table: "PrescriptionMedicament");
+
+            migrationBuilder.DropColumn(
+                name: "Dose",
+                table: "PrescriptionMedicament");
+        }
+    }
+}
